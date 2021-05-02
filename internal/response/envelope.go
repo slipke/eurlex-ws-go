6abcbe0fdbec91532b85a3ada5af3bff04492d0c
Simon Lipke <simon.lipke@googlemail.com>
2021-05-02 14:02:12 +0200

    [TASK] Refactored to use eurlex-model-go

--- internal/response/envelope.go
+++ internal/response/envelope.go
@@ -2,6 +2,8 @@ package response
 
 import (
 	"encoding/xml"
+
+	model "github.com/slipke/eurlex-model-go"
 )
 
 type Envelope struct {
@@ -11,37 +13,32 @@ type Envelope struct {
 }
 
 func NewEnvelopeFromXML(xmlStr string) (*Envelope, error) {
-	e := &Envelope{}
-	if err := xml.Unmarshal([]byte(xmlStr), e); err != nil {
+	var e *Envelope
+	if err := xml.Unmarshal([]byte(xmlStr), &e); err != nil {
 		return nil, err
 	}
 	return e, nil
 }
 
 type Header struct {
-	XMLName       xml.Name       `xml:"Header"`
 	NotUnderstood *NotUnderstood `xml:"NotUnderstood"`
 }
 
 type NotUnderstood struct {
-	XMLName xml.Name `xml:"NotUnderstood"`
-	Qname   string   `xml:"qname,attr"`
+	Qname string `xml:"qname,attr"`
 }
 
 type Body struct {
-	XMLName       xml.Name       `xml:"Body"`
 	Fault         *Fault         `xml:"Fault"`
 	SearchResults *SearchResults `xml:"searchResults"`
 }
 
 type Fault struct {
-	XMLName xml.Name `xml:"Fault"`
-	Code    string   `xml:"Code>Value"`
-	Reason  string   `xml:"Reason>Text"`
+	Code   string `xml:"Code>Value"`
+	Reason string `xml:"Reason>Text"`
 }
 
 type SearchResults struct {
-	XMLName   xml.Name  `xml:"searchResults"`
 	NumHits   int64     `xml:"numhits"`
 	TotalHits int64     `xml:"totalhits"`
 	Page      int64     `xml:"page"`
@@ -50,122 +47,11 @@ type SearchResults struct {
 }
 
 type Result struct {
-	XMLName   xml.Name `xml:"result"`
 	Reference string   `xml:"reference"`
 	Rank      string   `xml:"rank"`
 	Content   *Content `xml:"content"`
 }
 
 type Content struct {
-	XMLName xml.Name `xml:"content"`
-	Notice  *Notice  `xml:"NOTICE"`
-}
-
-type Notice struct {
-	XMLName    xml.Name    `xml:"NOTICE"`
-	Expression *Expression `xml:"EXPRESSION"`
-	Work       *NoticeWork `xml:"WORK"`
-}
-
-type Expression struct {
-	XMLName  xml.Name                `xml:"EXPRESSION"`
-	Title    string                  `xml:"EXPRESSION_TITLE>VALUE"`
-	Language *ExpressionUsesLanguage `xml:"EXPRESSION_USES_LANGUAGE"`
-}
-
-type ExpressionUsesLanguage struct {
-	XMLName xml.Name `xml:"EXPRESSION_USES_LANGUAGE"`
-	URI     *URI     `xml:"URI"`
-}
-
-type NoticeWork struct {
-	XMLName                                 xml.Name                                 `xml:"WORK"`
-	IDCelex                                 string                                   `xml:"ID_CELEX>VALUE"`
-	ResourceLegalInForce                    bool                                     `xml:"RESOURCE_LEGAL_IN-FORCE>VALUE"`
-	ResourceLegalPublishedInOfficialJournal *ResourceLegalPublishedInOfficialJournal `xml:"RESOURCE_LEGAL_PUBLISHED_IN_OFFICIAL-JOURNAL"`
-	WorkCreatedByAgent                      *WorkCreatedByAgent                      `xml:"WORK_CREATED_BY_AGENT"`
-	WorkDateDocument                        *WorkDateDocument                        `xml:"WORK_DATE_DOCUMENT"`
-	WorkHasExpression                       []*WorkHasExpression                     `xml:"WORK_HAS_EXPRESSION"`
-}
-
-type ResourceLegalPublishedInOfficialJournal struct {
-	XMLName        xml.Name        `xml:"RESOURCE_LEGAL_PUBLISHED_IN_OFFICIAL-JOURNAL"`
-	EmbeddedNotice *EmbeddedNotice `xml:"EMBEDDED_NOTICE"`
-	Samas          []*Sameas       `xml:"SAMEAS"`
-}
-
-type EmbeddedNotice struct {
-	XMLName xml.Name            `xml:"EMBEDDED_NOTICE"`
-	Work    *EmbeddedNoticeWork `xml:"WORK"`
-}
-
-type EmbeddedNoticeWork struct {
-	XMLName                                 xml.Name                                 `xml:"WORK"`
-	DatePublication                         string                                   `xml:"DATE_PUBLICATION>VALUE"`
-	OfficialJournalClass                    string                                   `xml:"OFFICIAL-JOURNAL_CLASS>VALUE"`
-	OfficialJournalNumber                   string                                   `xml:"OFFICIAL-JOURNAL_NUMBER>VALUE"`
-	OfficialJournalPartOfCollectionDocument *OfficialJournalPartOfCollectionDocument `xml:"OFFICIAL-JOURNAL_PART_OF_COLLECTION_DOCUMENT"`
-	OfficialJournalYear                     string                                   `xml:"OFFICIAL-JOURNAL_YEAR>VALUE"`
-}
-
-type OfficialJournalPartOfCollectionDocument struct {
-	XMLName xml.Name `xml:"OFFICIAL-JOURNAL_PART_OF_COLLECTION_DOCUMENT"`
-	URI     *URI     `xml:"URI"`
-}
-
-type Sameas struct {
-	XMLName xml.Name `xml:"SAMEAS"`
-	URI     *URI     `xml:"URI"`
-}
-
-type URI struct {
-	XMLName    xml.Name `xml:"URI"`
-	Identifier string   `xml:"IDENTIFIER"`
-	Type       string   `xml:"TYPE"`
-	Value      string   `xml:"VALUE"`
-}
-
-type WorkCreatedByAgent struct {
-	XMLName    xml.Name `xml:"WORK_CREATED_BY_AGENT"`
-	AltLabel   []string `xml:"ALTLABEL"`
-	CompactURI string   `xml:"COMPACT_URI"`
-	Identifier string   `xml:"IDENTIFIER"`
-	OpCode     string   `xml:"OP-CODE"`
-	PrefLabel  string   `xml:"PREFLABEL"`
-	URI        *URI     `xml:"URI"`
-}
-
-type WorkDateDocument struct {
-	XMLName xml.Name `xml:"WORK_DATE_DOCUMENT"`
-	Day     string   `xml:"DAY"`
-	Month   string   `xml:"MONTH"`
-	Year    string   `xml:"YEAR"`
-}
-
-type WorkHasExpression struct {
-	XMLName        xml.Name                         `xml:"WORK_HAS_EXPRESSION"`
-	EmbeddedNotice *WorkHasExpressionEmbeddedNotice `xml:"EMBEDDED_NOTICE"`
-}
-
-type WorkHasExpressionEmbeddedNotice struct {
-	XMLName       xml.Name                     `xml:"EMBEDDED_NOTICE"`
-	Expression    *WorkHasExpressionExpression `xml:"EXPRESSION"`
-	Manifestation *Manifestation               `xml:"MANIFESTATION"`
-}
-
-type WorkHasExpressionExpression struct {
-	XMLName                xml.Name `xml:"EXPRESSION"`
-	ExpressionUsesLanguage string   `xml:"EXPRESSION_USES_LANGUAGE>OP-CODE"`
-}
-
-type Manifestation struct {
-	XMLName                                   xml.Name                          `xml:"MANIFESTATION"`
-	ManifestationOfficialJournalPartPageFirst string                            `xml:"MANIFESTATION_OFFICIAL-JOURNAL_PART_PAGE_FIRST>VALUE"`
-	ManifestationOfficialJournalPartPageLast  string                            `xml:"MANIFESTATION_OFFICIAL-JOURNAL_PART_PAGE_LAST>VALUE"`
-	ManifestationPartOfManifestation          *ManifestationPartOfManifestation `xml:"MANIFESTATION_PART_OF_MANIFESTATION"`
-}
-
-type ManifestationPartOfManifestation struct {
-	XMLName xml.Name `xml:"MANIFESTATION_PART_OF_MANIFESTATION"`
-	Sameas  *Sameas  `xml:"SAMEAS"`
+	Notice *model.Notice `xml:"NOTICE"`
 }
--- search_result_test.go
+++ search_result_test.go
@@ -63,6 +63,25 @@ func TestCreateSearchResult(t *testing.T) {
 	// Check first result
 	res := sr.Result[0]
 
+	if len(res.Content.Notice.Expression) <= 0 {
+		t.Errorf("Failed to check expressions, 0 found")
+		t.FailNow()
+	}
+	e := res.Content.Notice.Expression[0]
+	w := res.Content.Notice.Work
+
+	if len(w.ResourceLegalPublishedInOfficialJournal) <= 0 {
+		t.Errorf("Failed to check ResourceLegalPublishedInOfficialJournal, 0 found")
+		t.FailNow()
+	}
+	rlpioj := w.ResourceLegalPublishedInOfficialJournal[0]
+
+	if len(w.WorkCreatedByAgent) <= 0 {
+		t.Errorf("Failed to check WorkCreatedByAgent, 0 found")
+		t.FailNow()
+	}
+	wcba := w.WorkCreatedByAgent[0]
+
 	checks = []struct {
 		Label    string
 		Value    interface{}
@@ -70,16 +89,16 @@ func TestCreateSearchResult(t *testing.T) {
 	}{
 		{"Result[0] Reference", res.Reference, "eng_cellar:2a2a817d-85b9-11e4-b8a5-01aa75ed71a1_en"},
 		{"Result[0] Rank", res.Rank, "1"},
-		{"Result[0] Notice Expression Title", res.Content.Notice.Expression.Title, "Commission Implementing Regulation (EU) No 1337/2014 of 16 December 2014 amending Implementing Regulations (EU) No 947/2014 and (EU) No 948/2014 as regards the last day for submission of applications for private storage aid for butter and skimmed milk powder"},
-		{"Result[0] Notice Expression Language", res.Content.Notice.Expression.Language.URI.Identifier, "ENG"},
-		{"Result[0] Notice Work IDCelex", res.Content.Notice.Work.IDCelex, "<em>32014R1337</em>"},
-		{"Result[0] Notice Work ResourceLegalInForce", res.Content.Notice.Work.ResourceLegalInForce, false},
-		{"Result[0] Notice Work ResourceLegalPublishedInOfficialJournal DatePUblication", res.Content.Notice.Work.ResourceLegalPublishedInOfficialJournal.EmbeddedNotice.Work.DatePublication, "2014-12-17"},
-		{"Result[0] Notice Work ResourceLegalPublishedInOfficialJournal len(SAMEAS)", len(res.Content.Notice.Work.ResourceLegalPublishedInOfficialJournal.Samas), 3},
-		{"Result[0] Notice Work WorkCreatedByAgent len(AltLAbel)", len(res.Content.Notice.Work.WorkCreatedByAgent.AltLabel), 4},
-		{"Result[0] Notice Work WorkCreatedByAgent Identifier", res.Content.Notice.Work.WorkCreatedByAgent.Identifier, "COM"},
-		{"Result[0] Notice Work WorkDateDocument Day", res.Content.Notice.Work.WorkDateDocument.Day, "16"},
-		{"Result[0] Notice Work len(HasExpression)", len(res.Content.Notice.Work.WorkHasExpression), 23},
+		{"Result[0] Notice Expression Title", e.ExpressionTitle, "Commission Implementing Regulation (EU) No 1337/2014 of 16 December 2014 amending Implementing Regulations (EU) No 947/2014 and (EU) No 948/2014 as regards the last day for submission of applications for private storage aid for butter and skimmed milk powder"},
+		{"Result[0] Notice Expression Language", e.ExpressionUsesLanguage.URI.Identifier, "ENG"},
+		{"Result[0] Notice Work IDCelex", w.IDCelex, "<em>32014R1337</em>"},
+		{"Result[0] Notice Work ResourceLegalInForce", w.ResourceLegalInForce, false},
+		{"Result[0] Notice Work ResourceLegalPublishedInOfficialJournal DatePublication", rlpioj.EmbeddedNotice.Work.DatePublication, "2014-12-17"},
+		{"Result[0] Notice Work ResourceLegalPublishedInOfficialJournal len(SAMEAS)", len(rlpioj.SameAs), 3},
+		{"Result[0] Notice Work WorkCreatedByAgent len(AltLAbel)", len(wcba.AltLabel), 4},
+		{"Result[0] Notice Work WorkCreatedByAgent Identifier", wcba.Identifier, "COM"},
+		{"Result[0] Notice Work WorkDateDocument Day", w.WorkDateDocument.Day, "16"},
+		{"Result[0] Notice Work len(HasExpression)", len(w.WorkHasExpression), 23},
 	}
 
 	for _, c := range checks {
@@ -88,16 +107,34 @@ func TestCreateSearchResult(t *testing.T) {
 		}
 	}
 
-	// Check the first expression
-	exp := res.Content.Notice.Work.WorkHasExpression[0]
+	if len(w.WorkHasExpression) <= 0 {
+		t.Errorf("Failed to check WorkHasExpression, 0 found")
+		t.FailNow()
+	}
+	// Check the first WorkHasExpression
+	whe := w.WorkHasExpression[0]
+
+	if len(whe.EmbeddedNotice.Expression) <= 0 {
+		t.Errorf("Failed to check whe.EmbeddedNotice.Expression, 0 found")
+		t.FailNow()
+	}
+	// Get inner expression
+	ie := whe.EmbeddedNotice.Expression[0]
+
+	if len(whe.EmbeddedNotice.Manifestation) <= 0 {
+		t.Errorf("Failed to check whe.EmbeddedNotice.Manifestation, 0 found")
+		t.FailNow()
+	}
+	// Get inner manifestation
+	im := whe.EmbeddedNotice.Manifestation[0]
 
 	checks = []struct {
 		Label    string
 		Value    interface{}
 		Expected interface{}
 	}{
-		{"Expression[0] EmbeddedNotice Expression Language", exp.EmbeddedNotice.Expression.ExpressionUsesLanguage, "ENG"},
-		{"Expression[0] EmbeddedNotice Manifestation PageFirst", exp.EmbeddedNotice.Manifestation.ManifestationOfficialJournalPartPageFirst, "0015"},
+		{"Expression[0] EmbeddedNotice Expression Language", ie.ExpressionUsesLanguage.OpCode, "ENG"},
+		{"Expression[0] EmbeddedNotice Manifestation PageFirst", im.ManifestationOfficialJournalPartPageFirst, "0015"},
 	}
 
 	for _, c := range checks {
